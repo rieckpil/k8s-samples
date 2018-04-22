41fe4db31f89a13d04b10b46fe772fe319b3b7dd
Philip Riecks <philip@riecks.io>
2018-04-22 17:40:33 +0200

    add simple java microservice pod definition

---
+++ simple-spring-boot-java-microservice/src/main/java/de/rieckpil/learning/k8s/k8ssample/HealthController.java
@@ -0,0 +1,15 @@
+package de.rieckpil.learning.k8s.k8ssample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public String getHealth() {
+        return "Up' and running!";
+    }
+}
